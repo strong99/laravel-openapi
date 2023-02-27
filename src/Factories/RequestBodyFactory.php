1a2b43f16e0a5d3f074fc4d30f01824cf21ad520
Rex van Dorp <rex@sandwave.io>
2023-02-27 18:06:14 +0100

    Fixed request body typo

--- src/Factories/RequestBodyFactory.php
+++ src/Factories/RequestBodyFactory.php
@@ -4,11 +4,11 @@ namespace Vyuldashev\LaravelOpenApi\Factories;
 
 use GoldSpecDigital\ObjectOrientedOAS\Objects\RequestBody;
 use Vyuldashev\LaravelOpenApi\Concerns\Referencable;
-use Vyuldashev\LaravelOpenApi\Attributes\Request as RequestAttribute;
+use Vyuldashev\LaravelOpenApi\Attributes\RequestBody as RequestBodyAttribute;
 
 abstract class RequestBodyFactory
 {
     use Referencable;
 
-    abstract public function build(RequestAttribute $attribute): RequestBody;
+    abstract public function build(RequestBodyAttribute $attribute): RequestBody;
 }
